1c4d87cf374698456c3927c688fe35f3cc653739
tejerka <tejerka@gmail.com>
2021-11-12 10:43:01 +0100

    Update CI

--- server/App.js
+++ server/App.js
@@ -1,8 +1,12 @@
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 
-function handler(req) {
-  return new Response("Hello world");
+const handler = async (req) => {
+  const file = await Deno.readFile("./client/dist/index.html");
+  return new Response(file, {
+    headers: {
+      "content-type": "text/css",
+    },
+  });
 }
 
-console.log("Listening on http://localhost:8000");
 await listenAndServe(":8000", handler);
