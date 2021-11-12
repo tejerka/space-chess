33ccb3db5d885476b5fd1959f8bb18572b8e3260
tejerka <tejerka@gmail.com>
2021-11-12 12:06:43 +0100

    Update route

--- server/App.js
+++ server/App.js
@@ -3,9 +3,8 @@ import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 const handler = async (req) => {
   const { pathname } = new URL(req.url);
 
-  console.log({pathname, cond: pathname === '/main.js'});
   if (pathname === '/main.js') {
-    new Response(await Deno.readFile("./client/dist/main.js"), {
+    return new Response(await Deno.readFile("./client/dist/main.js"), {
       headers: {
         "content-type": "application/javascript",
       },
@@ -13,7 +12,7 @@ const handler = async (req) => {
   }
 
   if (pathname === '/manifest.json') {
-    new Response(await Deno.readFile("./client/dist/manifest.json"), {
+    return new Response(await Deno.readFile("./client/dist/manifest.json"), {
       headers: {
         "content-type": "application/json",
       },
